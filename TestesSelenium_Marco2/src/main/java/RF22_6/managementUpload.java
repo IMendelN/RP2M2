527a66725a07f328d6596e4c10800f0ec9591fca
Dionas Luan Muller <dionasluanmuller3@gmail.com>
2022-03-19 14:19:29 -0300

    Add mais um arquivo esquecido...

---
+++ TestesSelenium_Marco2/src/main/java/RF22_6/managementUpload.java
@@ -0,0 +1,45 @@
+package RF22_6;
+
+import org.junit.After;	
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import Adriel.DSL;
+import Adriel.SilverBulletPage;
+
+	public class managementUpload {
+		
+		private DSL dsl;
+		private SilverBulletPage page;
+
+		@Before
+		public final void setUp() throws Exception {
+			System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().setPosition(new Point(100, 100));
+			driver.manage().window().setSize(new Dimension(1500, 1500));
+			new DSL(driver);
+			page = new SilverBulletPage(driver);
+			dsl = new DSL(driver);
+		}
+
+		@After
+		public void Finalizar() {
+			dsl.close();
+		}
+		
+		@Test
+		public void FluxoPrincipal() throws InterruptedException {
+
+			page.login();
+
+			page.openProject();
+			
+			page.EarnedValue();
+			
+			page.UploadManegement();
+		}
+}
